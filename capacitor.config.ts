a82a7b712249c0c8f772176eebaf228f4c2a73ac
Eric Antony Vinhaes Prohmann <eric.prohmann@ifma.edu.br>
2022-02-15 19:39:30 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'primeiroapp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
