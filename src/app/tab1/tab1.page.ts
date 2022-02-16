b3dee7ae04d13a83465f2c57163eb7bc2fd7420c
Eric Antony Vinhaes Prohmann <eric.prohmann@ifma.edu.br>
2022-02-15 23:37:20 -0300

    criei o giff

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/quotes */
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,8 +7,28 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
+
+
 export class Tab1Page {
+  src= "../../assets/img/pikachu1.svg";
+  img_rotator=0;
 
   constructor() {}
+  ngOnInit(){
+    setInterval(() =>{
+      if(this.img_rotator===0)
+        {this.src = "../../assets/img/pikachu1.svg";}
+      if(this.img_rotator===1)
+        {this.src = "../../assets/img/pikachu2.svg";}
+      if(this.img_rotator===2)
+        {this.src = "../../assets/img/pikachu3.svg";}
+      this.img_rotator++;
+      if(this.img_rotator>=3)
+        {this.img_rotator=0;}
+    }, 500);
+  }
+
+
 
 }
