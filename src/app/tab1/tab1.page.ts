e2039ce3ef748e07789f893ae64e034537e3717c
Eric Antony Vinhaes Prohmann <eric.prohmann@ifma.edu.br>
2022-02-17 09:33:57 -0300

    Mudando o giff para a tab2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -11,24 +11,6 @@ import { Component } from '@angular/core';
 
 
 export class Tab1Page {
-  src= "../../assets/img/pikachu1.svg";
-  img_rotator=0;
-
-  constructor() {}
-  ngOnInit(){
-    setInterval(() =>{
-      if(this.img_rotator===0)
-        {this.src = "../../assets/img/pikachu1.svg";}
-      if(this.img_rotator===1)
-        {this.src = "../../assets/img/pikachu2.svg";}
-      if(this.img_rotator===2)
-        {this.src = "../../assets/img/pikachu3.svg";}
-      this.img_rotator++;
-      if(this.img_rotator>=3)
-        {this.img_rotator=0;}
-    }, 500);
-  }
-
-
+  constructor(){}
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  src= "../../assets/img/pikachu1.svg";
+  img_rotator=0;
 
   constructor() {}
+  ngOnInit(){
+    setInterval(() =>{
+      if(this.img_rotator===0)
+        {this.src = "../../assets/img/pikachu1.svg";}
+      if(this.img_rotator===1)
+        {this.src = "../../assets/img/pikachu2.svg";}
+      if(this.img_rotator===2)
+        {this.src = "../../assets/img/pikachu3.svg";}
+      this.img_rotator++;
+      if(this.img_rotator>=3)
+        {this.img_rotator=0;}
+    }, 500);
+  }
 
 }
