3753eaed4f714af2462d54fa0ddb137f343465bd
Eric Antony Vinhaes Prohmann <eric.prohmann@ifma.edu.br>
2022-02-17 10:57:54 -0300

    Atualizacao no giff do bixinho para que varias aparencias de bixinho possam ser utilizadas. Criacao do croqui

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,22 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  src= "../../assets/img/pikachu1.svg";
-  img_rotator=0;
+  bixinho = 1; //esta linha indica o bixinho escolhido. Futuramente
+  //iremos buscar este valor da classe em uma variavel chamada aparencia.
+  src = "../../assets/img/bixinho"+String(this.bixinho)+"_posicao1.svg";
+  img_rotator = 0;
+  temporizador: any;
+
 
   constructor() {}
-  ngOnInit(){
-    setInterval(() =>{
+  ngOnInit() {
+    this.temporizador = setInterval(() =>{
+
       if(this.img_rotator===0)
-        {this.src = "../../assets/img/pikachu1.svg";}
+        {this.src = "../../assets/img/bixinho"+String(this.bixinho)+"_posicao1.svg";}
       if(this.img_rotator===1)
-        {this.src = "../../assets/img/pikachu2.svg";}
+        {this.src = "../../assets/img/bixinho"+String(this.bixinho)+"_posicao2.svg";}
       if(this.img_rotator===2)
-        {this.src = "../../assets/img/pikachu3.svg";}
+        {this.src = "../../assets/img/bixinho"+String(this.bixinho)+"_posicao3.svg";}
       this.img_rotator++;
       if(this.img_rotator>=3)
         {this.img_rotator=0;}
-    }, 500);
+    }, 700);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.temporizador);
   }
 
 }
