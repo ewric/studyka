15560a4a292d4f56a797d03a2736017ab35eefe4
Eric Antony Vinhaes Prohmann <eric.prohmann@ifma.edu.br>
2022-02-19 14:28:13 -0300

    Adicao de funcao que calcula diferenca em horas entre data passada e atual na classe bixinho.

--- src/app/models/IBixinho.model.ts
+++ src/app/models/IBixinho.model.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 export class Bixinho {
   nome = "Meu Bixinho";
   inapetencia = 60; //valor em %
@@ -5,18 +6,24 @@ export class Bixinho {
   experiencia = 0; //valor em %
   level = 1;
   experiencia_proximo_level = 100;
-  experiencia_porcentagem = experiencia/this.experiencia_proximo_level;
+  experiencia_porcentagem = this.experiencia/this.experiencia_proximo_level;
   dinheiro = 150;
+  aparencia = 1;
+  ultimaReducaoInapetencia: Date;
 
   constructor() {}
 
-  aumentaInapetencia(number: comida){
+  aumentaInapetencia(comida: number){
     this.inapetencia += comida;
     if(this.inapetencia > 100)
       {this.inapetencia=100;}
   }
 
 
+  diffEmHorasDeDataPassadaParaAtual(dataPassada: Date) {
+    const HOURSINms = 60*60*1000;
+    return Math.floor((Date.now() - Date.parse(dataPassada.toISOString())) / HOURSINms)+1;
+  }
 
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
+import { Bixinho } from './../models/IBixinho.model';
 import { Component } from '@angular/core';
+import { IonDatetime } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -6,23 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  bixinho = 1; //esta linha indica o bixinho escolhido. Futuramente
-  //iremos buscar este valor da classe em uma variavel chamada aparencia.
-  src = "../../assets/img/bixinho"+String(this.bixinho)+"_posicao1.svg";
+  bixinho = new Bixinho;
+  src = "../../assets/img/bixinho"+String(this.bixinho.aparencia)+"_posicao1.svg";
   img_rotator = 0;
   temporizador: any;
 
+  ultimoAcesso = new Date(2022,0,18);
+  diffDias: number;
+
 
   constructor() {}
+
   ngOnInit() {
     this.temporizador = setInterval(() =>{
-
       if(this.img_rotator===0)
-        {this.src = "../../assets/img/bixinho"+String(this.bixinho)+"_posicao1.svg";}
+        {this.src = "../../assets/img/bixinho"+String(this.bixinho.aparencia)+"_posicao1.svg";}
       if(this.img_rotator===1)
-        {this.src = "../../assets/img/bixinho"+String(this.bixinho)+"_posicao2.svg";}
+        {this.src = "../../assets/img/bixinho"+String(this.bixinho.aparencia)+"_posicao2.svg";}
       if(this.img_rotator===2)
-        {this.src = "../../assets/img/bixinho"+String(this.bixinho)+"_posicao3.svg";}
+        {this.src = "../../assets/img/bixinho"+String(this.bixinho.aparencia)+"_posicao3.svg";}
       this.img_rotator++;
       if(this.img_rotator>=3)
         {this.img_rotator=0;}
@@ -32,5 +36,6 @@ export class Tab2Page {
   ngOnDestroy() {
     clearInterval(this.temporizador);
   }
+
 
 }
