8d219b1d2a7bb0beb3d2f4caea158351c10bc6ff
Eric Antony Vinhaes Prohmann <eric.prohmann@ifma.edu.br>
2022-02-21 11:18:52 -0300

    Criacao de funcoes na classe bixinho. Falta funcoes temporais

--- src/app/models/IBixinho.model.ts
+++ src/app/models/IBixinho.model.ts
@@ -1,29 +1,133 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 export class Bixinho {
+  private BRINQUEDO = 0.05; //quanto que um brinquedo dá de felicidade
+  private COMIDA = 0.05; //quanto que uma comida dá de felicidade
+  private DINHEIRO_INICIAL = 150; //dinheiro em que se inicia o jogo
+  private EXPERIENCIA_PRIMEIRO_LEVEL = 100;
+  CUSTO_COMIDA = 25;
+  CUSTO_BRINQUEDO = 25;
+
   nome = "Meu Bixinho";
   inapetencia = 0.6;
   felicidade = 0.6;
-  experiencia = 50;
+  experiencia = 0;
   level = 1;
-  experiencia_proximo_level = 100;
+  experiencia_proximo_level = this.EXPERIENCIA_PRIMEIRO_LEVEL;
   experiencia_normalizada = (this.experiencia/this.experiencia_proximo_level);
   experiencia_porcentagem = ((this.experiencia/this.experiencia_proximo_level*100).toFixed(2));
-  dinheiro = 150;
+  dinheiro = this.DINHEIRO_INICIAL;
   aparencia = 1;
-  ultimaReducaoInapetencia: Date;
+  ultimaReducaoInapetencia = new Date(2022,01,18);
+
+
+
 
   constructor() {}
-
-  aumentaInapetencia(comida: number){
-    this.inapetencia += comida;
-    if(this.inapetencia > 100)
-      {this.inapetencia=100;}
+//###################FUNCOES QUE AUMENTAM OU DIMINUEM ALGUMA VARIAVEL################################
+  aumentaInapetencia(aumento: number) {
+    this.inapetencia += aumento;
+    if(this.inapetencia >= 1) this.inapetencia = 1;
+  }
+  aumentaFelicidade(aumento: number) {
+    this.felicidade += aumento;
+    if(this.felicidade >= 1) this.felicidade = 1;
+    this.aumentaExperiencia();
+  }
+  aumentaDinheiro() {
+    this.dinheiro += Math.round(Math.random()*10 + 10);
+  }
+  diminuiInapetencia(diminuicao: number) {
+    this.inapetencia -= diminuicao;
+    if(this.inapetencia <= 0) this.morte();
+  }
+  diminuiFelicidade(diminuicao: number) {
+    this.felicidade -= diminuicao;
+    if(this.felicidade<= 0) this.felicidade = 0;
   }
 
 
+
+
+//###################FUNCOES RELACIONADAS A LEVEL################################
+  aumentaExperiencia() {
+    if(this.inapetencia >= 0.8 && this.felicidade >= 0.6) {
+      this.experiencia += Math.round(Math.random()*10)+10;
+      this.atualizaExperiencia();
+    }
+    if(this.experiencia>=this.experiencia_proximo_level) this.passouDeLevel();
+  }
+  atualizaExperiencia() {
+    this.experiencia_normalizada = (this.experiencia/this.experiencia_proximo_level);
+    this.experiencia_porcentagem = ((this.experiencia/this.experiencia_proximo_level*100).toFixed(2));
+  }
+  atualizaExperienciaProximoLevel() {
+    this.experiencia_proximo_level = 100 + this.level*40;
+  }
+  passouDeLevel() {
+    this.level += 1;
+    this.experiencia = 0;
+    this.atualizaExperienciaProximoLevel();
+    this.atualizaExperiencia();
+  }
+  morte() {
+    this.inapetencia = 0.6;
+    this.felicidade = 0.6;
+    this.experiencia = 0;
+    this.atualizaExperienciaProximoLevel();
+    if(this.level <= 0){
+      this.level = 1;
+      this.ultimaReducaoInapetencia = Date.now();
+      this.dinheiro = 150;
+      this.experiencia_proximo_level = 100;
+    }
+  }
+
+
+
+//###################FUNCOES DE HABITOS################################
+  bomHabito() {
+    this.aumentaFelicidade(Math.random()/20);
+    this.aumentaDinheiro();
+    this.aumentaExperiencia();
+  }
+  mauHabito() {
+    this.felicidade -= Math.round(Math.random()*8+2)/100;
+    if(this.felicidade <= 0) this.felicidade = 0;
+  }
+
+
+
+
+
+
+//###################COMPRA DE ITENS################################
+  comprouBrinquedo() {
+    if(this.dinheiro-this.CUSTO_BRINQUEDO >= 0) {
+      this.aumentaFelicidade(this.BRINQUEDO);
+      this.dinheiro -= this.CUSTO_BRINQUEDO;
+    }
+  }
+
+  comprouComida() {
+    if(this.dinheiro-this.CUSTO_COMIDA >= 0) {
+      this.aumentaInapetencia(this.COMIDA);
+      this.dinheiro -= this.CUSTO_COMIDA;
+    }
+  }
+
+
+//###################FUNCOES RELACIONADAS COM TEMPO################################
   diffEmHorasDeDataPassadaParaAtual(dataPassada: Date) {
-    const HOURSINms = 60*60*1000;
+    const HOURSINms = 60*1000;//60*60*1000;
+    console.log(dataPassada);
     return Math.floor((Date.now() - Date.parse(dataPassada.toISOString())) / HOURSINms)+1;
+  }
+
+  tempoPassou() {
+    if(this.diffEmHorasDeDataPassadaParaAtual(this.ultimaReducaoInapetencia) > 1){
+      const diffEmHoras = this.diffEmHorasDeDataPassadaParaAtual(this.ultimaReducaoInapetencia);
+      this.diminuiInapetencia(diffEmHoras/100);
+    }
   }
 
 
