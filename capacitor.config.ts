eabf9cf69808fd9271ab75925cf28851be6e1b9e
Eric Antony Vinhaes Prohmann <eric.prohmann@ifma.edu.br>
2022-03-24 21:39:52 -0300

    Dark mode deletado.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'primeiroapp',
+  appName: 'Studyka',
   webDir: 'www',
   bundledWebRuntime: false
 };
